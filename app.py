cea1e655b3f4f0784b4d2fcdab3a285b197434e6
Elifia Muthia <elifiamuthia@Elifias-Air.lan>
2023-07-30 22:26:53 -0400

    added cors configuration

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import requests
 import scraper
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+# def index():
+    # return render_template('index.html')
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
     data = []
-    url = request.json.get('url')
+    # url = request.json.get('url')
+    url = "https://www.goodreads.com/review/list/116631395-elifia?ref=nav_mybooks&shelf=read"
     print(url)
 
     books = scraper.scrape(url)
